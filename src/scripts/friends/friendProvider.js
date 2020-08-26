f32ed8ecc523da512d4f6fc09b63d3684d257ef7
mgjeffries <gib.jeffries@gmail.com>
2020-08-26 09:51:41 -0500

    add functions to friend provider

--- src/scripts/friends/friendProvider.js
+++ src/scripts/friends/friendProvider.js
@@ -1,6 +1,12 @@
+const eventHub = document.querySelector(".container")
+const friendChange = new CustomEvent("friendChange")
 let friends = []
 
 export const useFriends = () => friends.slice()
+
+export const useFriendsByUserId = userId => {
+  return friends.filter( f => f.id === userId)
+}
 
 export const getFriends = () => {
   return fetch("http://localhost:8088/friends")
@@ -8,3 +14,18 @@ export const getFriends = () => {
     .then(friendData => friends = friendData)
 }
 
+export const saveFriend = friendData => {
+  const jsonEntry = JSON.stringify(friendData)
+
+  return fetch("http://localhost:8088/friends", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: jsonEntry
+  })
+    .then(getFriends)
+    .then(dispatchChangeEvent)
+}
+
+const dispatchChangeEvent = () => eventHub.dispatchEvent(friendChange)
