82c3938348a07d82447834b7685b5ca4158f6905
Austin Ring <aring97@gmail.com>
2020-08-26 15:39:05 -0500

    form working as mobal

--- src/scripts/events/EventForm.js
+++ src/scripts/events/EventForm.js
@@ -4,19 +4,39 @@ const eventHub=document.querySelector(".container")
 const contentTarget=document.querySelector(".event-button")
 
 export const eventForm=()=>{
-    contentTarget.innerHTML=`<button class="btn" id="create--event">New event</button>`
+    contentTarget.innerHTML=`<button id="eventBtn" class="btn">New event</button>
+    <div id="event--form" class="event--mobal" />
+    <div class="event--form__content">
+    <p>Name of event:</p>
+    <input type="text" class="event--form__input" id="eventName">
+    <p>Date of  event:</p>
+    <input type="date" class="event--form__input" id="eventDate">
+    <p>Location of event:</p>
+    <input type="text" class="event--form__input" id="eventLocation">
+    <button id="event--form__close" class="close">Close event form</button>
+    <button id="create--event">submit</button>
+    </div>
+    </div>
+    `
 }
 
 eventHub.addEventListener("click", event=>{
+    if(event.target.id==="eventBtn"){
+        const mobal=document.querySelector("#event--form")
+        mobal.style.display="block"
+    }else if(event.target.id=="event--form__close"){
+        const mobal=document.querySelector("#event--form")
+        mobal.style.display="none"
+    }
     if(event.target.id==="create--event"){
-        const eventName=prompt("please enter the name of the event")
-        const eventDate=prompt("please enter the date of the event","MM/DD/YYYY")
-        const eventLocation=prompt("please enter the locatoin of the event")
-        if(eventName===null||eventName===""){
+        const eventName=document.querySelector("#eventName").value
+        const eventDate=document.querySelector("#eventDate").value
+        const eventLocation=document.querySelector("#eventLocation").value
+        if(eventName===""){
             alert("Failed to enter an event name")
-        }else if(eventDate===null||eventDate==="MM/DD/YYYY"){
+        }else if(eventDate===""){
             alert("Failed to enter an event date")
-        }else if(eventLocation===null||eventLocation===""){
+        }else if(eventLocation===""){
             alert("Failed to enter an event location")
         }else{
             const eventObject={
