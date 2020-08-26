c609fd8dbc4a2de43d0ae5405bf0a668e3854e89
heymonicakay <hey.monicakay@gmail.com>
2020-08-26 10:55:02 -0500

    updated eventHub target and renamed articles directory

--- src/scripts/articles/ArticleProvider.js
+++ src/scripts/articles/ArticleProvider.js
@@ -1,6 +1,6 @@
 let articles = []
 
-const eventHub = document.querySelector(".main")
+const eventHub = document.querySelector(".news-container")
 
 const dispatchArticlesStateChangeEvent = () => {
     const articlesStateChangedEvent = new CustomEvent("articlesStateChanged")
@@ -15,7 +15,7 @@ export const saveArticle = (article) => {
   return fetch('http://localhost:8088/articles', {
     method: "POST",
     headers: {
-        "Content-Type": "application/json"
+      "Content-Type": "application/json"
     },
     body: jsonNote
   })
