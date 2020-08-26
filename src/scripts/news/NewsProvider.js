dcfff18e71dd9d45846d830f78612aecaed6c27f
heymonicakay <hey.monicakay@gmail.com>
2020-08-26 10:18:13 -0500

    added news data provider module

---
+++ src/scripts/news/NewsProvider.js
@@ -0,0 +1,36 @@
+let articles = []
+
+const eventHub = document.querySelector(".main")
+
+const dispatchNewsStateChangeEvent = () => {
+    const newsStateChangedEvent = new CustomEvent("newsStateChanged")
+
+    eventHub.dispatchEvent(newsStateChangedEvent)
+}
+
+export const saveArticle = (article) => {
+
+  const jsonNote = JSON.stringify(article)
+
+  return fetch('http://localhost:8088/articles', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: jsonNote
+  })
+    .then(getNews)
+      .then(dispatchNewsStateChangeEvent)
+}
+
+export const useNews = () => {
+  return articles.slice()
+}
+
+export const getNews = () => {
+  return fetch('http://localhost:8088/articles')
+    .then(response => response.json())
+      .then(parsedNews => {
+        articles = parsedNews
+      })
+}
