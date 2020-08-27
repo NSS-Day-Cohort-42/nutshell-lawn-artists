ae905c48c2707d80ba6050c0567da6ee91cb9ca7
Austin Ring <aring97@gmail.com>
2020-08-27 09:47:04 -0500

    event mobal closed when click outside of box

--- src/scripts/events/EventForm.js
+++ src/scripts/events/EventForm.js
@@ -7,8 +7,7 @@ const contentTarget=document.querySelector(".popup-container")
 export const eventForm=()=>{
     buttonTarget.innerHTML=`<button id="eventBtn" class="btn">New event</button>`
     contentTarget.innerHTML=`
-    <div id="event--form" class="event--mobal" />
-    <div class="event--form__content">
+    <div id="event--form__content" class="event--form__content">
     <p>Name of event:</p>
     <input type="text" class="event--form__input" id="eventName">
     <p>Date of  event:</p>
@@ -18,22 +17,29 @@ export const eventForm=()=>{
     <button id="event--form__close" class="close">Close event form</button>
     <button id="create--event">submit</button>
     </div>
-    </div>
     `
 }
 
 eventHub.addEventListener("click", event=>{
+    const mobal=document.querySelector(".popup-container")
+    window.onclick=(windowEvent)=>{
+        console.log(windowEvent)
+        if(windowEvent.target.id===""&&windowEvent.target.localName!=="p"){
+            mobal.style.display="none"    
+        }
+    }
     if(event.target.id==="eventBtn"){
-        const mobal=document.querySelector("#event--form")
         mobal.style.display="block"
     }else if(event.target.id=="event--form__close"){
-        const mobal=document.querySelector("#event--form")
+        mobal.style.display="none"
+    }else if(event.target.id==="window"){
         mobal.style.display="none"
     }
-    if(event.target.id==="create--event"){
+    else if(event.target.id==="create--event"){
         const eventName=document.querySelector("#eventName").value
         const eventDate=document.querySelector("#eventDate").value
         const eventLocation=document.querySelector("#eventLocation").value
+        console.log(document.querySelector("#eventDate"))
         if(eventName===""){
             alert("Failed to enter an event name")
         }else if(eventDate===""){
