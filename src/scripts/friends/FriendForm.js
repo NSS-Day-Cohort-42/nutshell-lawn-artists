c7fe22072574866dea28f0c6b9e36d5ed3d1ec08
mgjeffries <gib.jeffries@gmail.com>
2020-08-27 09:36:02 -0500

    identify buttons by id

--- src/scripts/friends/FriendForm.js
+++ src/scripts/friends/FriendForm.js
@@ -9,8 +9,7 @@ eventHub.addEventListener("friendAction", customEvent => {
 })
 
 eventHub.addEventListener("click", clickEvent => {
-  debugger
-  if (clickEvent.target.classList.contains("cancel-friend-change")) {
+  if (clickEvent.target.id === "cancel-friend-change") {
     contentTarget.classList.remove("visible")
   }
 })
@@ -19,14 +18,14 @@ const render = targetUserID => {
 
   if (isFriendOfCurrentUser(targetUserID)) {
     contentTarget.innerHTML = `
-    <button class="btn remove-friend--${targetUserID}">Remove Friend</button>
-    <button class="btn cancel-friend-change">Cancel</button>
+    <button class="btn" id="remove-friend--${targetUserID}">Remove Friend</button>
+    <button class="btn" id="cancel-friend-change">Cancel</button>
   `
   }
   else {
     contentTarget.innerHTML = `
-    <button class="add-friend--${targetUserID}">Add Friend</button>
-    <button class="btn cancel-friend-change>Cancel</button>
+    <button class="btn" id="add-friend--${targetUserID}">Add Friend</button>
+    <button class="btn" id="cancel-friend-change">Cancel</button>
   `
   }
 
