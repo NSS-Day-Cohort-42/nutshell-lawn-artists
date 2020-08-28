5f988b90ac1958ee04b285e306fb5c7a729374f7
Austin Ring <aring97@gmail.com>
2020-08-28 11:32:56 -0500

    logic to display events properly

--- src/scripts/events/EventList.js
+++ src/scripts/events/EventList.js
@@ -1,44 +1,70 @@
 import {getEvents, useEvents} from "./EventProvider.js"
 import {eventHTML} from "./EventCard.js"
-import {useFriendsByUserId, getFriends} from "../friends/FriendProvider.js"
+import {useFriendsByUserId, getFriends, useFriends} from "../friends/FriendProvider.js"
 
 const contentTarget=document.querySelector(".event-container")
 
 export const eventList=()=>{
     //get events then get friends
     getEvents()
-    .then(getFriends())
+    .then(getFriends)
     .then(()=>{
-        //assign friends to the friendsArray 
-        const friendsArray=useFriendsByUserId(parseInt(sessionStorage.getItem("activeUser")))
+        //assign friends to the friendsArray
+        const friendsArray=useFriendsByUserId(sessionStorage.getItem("activeUser"))
         //assign events to eventsArray
         let eventArray=useEvents()
         /*convert the date string in the event object into a date object.
         Then assign the date object to the date key of the event object*/
-        eventArray=eventArray.map(eventObj=>{
+        if(eventArray.length!==0){
+            eventArray=eventArray.map(eventObj=>{
             const dateObj=new Date(eventObj.date)
             eventObj.date=dateObj
             return eventObj
         })
         /*sorting the eventObject based on date then reverse it*/
         const eventsSortedByDate=eventArray.sort((firstObj, secondObj)=>{
-            console.log(secondObj.date-firstObj.date)
             return secondObj.date-firstObj.date
         }).reverse()
-
-        const correctEvents=eventsSortedByDate.map(eventObj=>{
+        let correctEvents=[]
+        if(friendsArray.length===0){
+            correctEvents=eventsSortedByDate.filter(eventObj=>eventObj.userId===parseInt(sessionStorage.getItem("activeUser")))
+        }else{
+            eventsSortedByDate.map(eventObj=>{
+                if(eventObj.userId===parseInt(sessionStorage.getItem("activeUser"))){
+                    correctEvents.push(eventObj)
+                }else(
+                    friendsArray.map(friendObj=>{
+                    if(eventObj.userId===friendObj.following){
+                        correctEvents.push(eventObj)
+                    }
+                }) 
+                
+                )
+        })
+        }
+        /*const correctEvents=eventsSortedByDate.map(eventObj=>{
             let currentEvent={}
-            if(friendsArray===[]){
+            if(friendsArray===undefined||friendsArray.length===0){
                 if(eventObj.userId===parseInt(sessionStorage.getItem("activeUser"))){
                     currentEvent=eventObj
                 }
-            }else{if(eventObj.userId===parseInt(sessionStorage.getItem("activeUser"))||eventObj.userId===friendObj.following){
-                currentEvent=eventObj
-            }
-        }
+            }else{
+                friendsArray.map(friendObj=>{
+                    if(eventObj.userId===parseInt(sessionStorage.getItem("activeUser"))||eventObj.userId===friendObj.following){
+                        console.log(eventObj)
+                        currentEvent=eventObj
+                }
+    })
+                }
         return currentEvent
-        })
-        contentTarget.innerHTML=correctEvents.map(eventObj=>eventHTML(eventObj)).join("")
+        })*/
+        console.log(correctEvents)
+        if (correctEvents===undefined||correctEvents.length===0){
+            contentTarget.innerHTML=`
+            <p>There are no events right now</p>
+            `    
+        }else{contentTarget.innerHTML=correctEvents.map(eventObj=>eventHTML(eventObj)).join("")}
+}
 })
 }
 
