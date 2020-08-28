4e3096f3d4db26c496cb6cc5b9b864edcc42b667
mgjeffries <gib.jeffries@gmail.com>
2020-08-28 11:53:06 -0500

    add class to show if message belongs to active user

--- src/scripts/messages/MessageCard.js
+++ src/scripts/messages/MessageCard.js
@@ -1,10 +1,17 @@
 import { FriendCard } from "../friends/FriendCard.js"
 
 export const MessageCard = message => {
-  
+  const activeUser = parseInt(sessionStorage.activeUser)
+  let messageOwner = ""
+  if(message.userId === activeUser){
+    messageOwner = "active-user-message"
+  }
+  else{
+    messageOwner = "other-user-message"
+  }
   
   return `
-    <div class="message-bubble">
+    <div class="message-bubble ${messageOwner}">
       ${FriendCard(message.userId)}
       <span class="message-text">: </span>
       <span class="message-text">${message.message}</span>
