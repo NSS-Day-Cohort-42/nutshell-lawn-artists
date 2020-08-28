c6e98847fa9f2e33356ab2d9b131fe472c37f247
mgjeffries <gib.jeffries@gmail.com>
2020-08-28 13:26:23 -0500

    fix caps

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -2,7 +2,7 @@ import { eventForm } from "./events/EventForm.js"
 import { TaskList } from "./tasks/TaskList.js";
 import { ListFriends } from "./friends/FriendList.js"
 import { ListArticles } from "./articles/ArticleList.js"
-import { ListMessages } from "./messages/messageList.js";
+import { ListMessages } from "./messages/MessageList.js";
 import "./tasks/TaskForm.js";
 import "./friends/FriendForm.js"
 import "./articles/ArticleForm.js"
