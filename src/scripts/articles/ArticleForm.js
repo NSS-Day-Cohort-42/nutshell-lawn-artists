9aab4d3560cb298aa816c7320d295059335532e7
heymonicakay <hey.monicakay@gmail.com>
2020-08-28 09:23:35 -0500

    fixed save article issue

--- src/scripts/articles/ArticleForm.js
+++ src/scripts/articles/ArticleForm.js
@@ -6,9 +6,9 @@ const popup = document.querySelector(".popup-container")
 eventHub.addEventListener("click", e => {
 
   if(e.target.classList.contains("btn-save-art")) {
-    const articleTitle = document.querySelector(".art-title")
-    const articleSynopsis = document.querySelector(".art-synopsis")
-    const articleUrl = document.querySelector(".art-url")
+    const articleTitle = document.querySelector(".art-title-input")
+    const articleSynopsis = document.querySelector(".art-synopsis-input")
+    const articleUrl = document.querySelector(".art-url-input")
 
     const newArticle = {
       userId: parseInt(sessionStorage.getItem("activeUser")),
@@ -24,7 +24,7 @@ eventHub.addEventListener("click", e => {
     popup.classList.remove("visible")
   }
 
-  else if (e.target.id === "createArticleBtn") {
+  else if (e.target.classList.contains("btn-create-article")) {
     popup.classList.add("visible")
     renderArticleForm()
   }
@@ -41,9 +41,9 @@ const renderArticleForm = () => {
 
   popup.innerHTML = `
     <section class="form-create-art">
-        <input type="text" class="art-title" id="articleTitle" placeholder="Article title"></input>
-        <input type="text" class="art-synopsis" id="articleSynopsis" placeholder="Article Synopsis"></input>
-        <input type="url" class="art-url" id="articleUrl" placeholder="Article URL"></input>
+        <input type="text" class="art-title-input" id="articleTitle" placeholder="Article title"></input>
+        <input type="text" class="art-synopsis-input" id="articleSynopsis" placeholder="Article Synopsis"></input>
+        <input type="url" class="art-url-input" id="articleUrl" placeholder="Article URL"></input>
         <button class="btn btn-save-art" id="saveArticle">Save Article</button>
         <button class="btn btn-close-form" id="closeForm">Cancel</button>
     </section>
